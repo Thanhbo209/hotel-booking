e0db13ad947c7554032e5591c63431b7659747c9
Thanh Pham <thanhagar123@gmail.com>
2026-02-10 10:46:19 +0700

    (frontend): implement zod validate

--- frontend/app/(auth)/register/layout.tsx
+++ frontend/app/(auth)/register/layout.tsx
@@ -1,8 +1,8 @@
 import RegisterForm from "@/app/(auth)/register/page";
 const layout = () => {
   return (
-    <div className="flex flex-col p-6 md:p-10 justify-center items-center min-h-svh">
-      <div className="w-full max-w-sm md:max-w-4xl bg-gradient-orange">
+    <div className="flex flex-col p-6 md:p-10 justify-center items-center min-h-svh absolute bg-gradient-orange inset-0 z-0">
+      <div className="w-full max-w-sm md:max-w-4xl ">
         <RegisterForm />
       </div>
     </div>
--- frontend/app/(auth)/register/page.tsx
+++ frontend/app/(auth)/register/page.tsx
@@ -1,10 +1,34 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Field, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import Image from "next/image";
 import Link from "next/link";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const registerSchema = z.object({
+  firstName: z.string().min(1, "You must type in your first name!"),
+  lastName: z.string().min(1, "You must type in your last name!"),
+  email: z.email("Email is not valid"),
+  password: z.string().min(6, "Password must has at least 6 characters"),
+});
+
+type RegisterFormValues = z.infer<typeof registerSchema>;
 
 const RegisterForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<RegisterFormValues>({
+    resolver: zodResolver(registerSchema),
+  });
+
+  const onSubmit = async (data: RegisterFormValues) => {};
+
   return (
     <section className="grid grid-cols-1 md:grid-cols-2 rounded-xl">
       <div className=" flex flex-col p-8 text-center">
@@ -18,29 +42,65 @@ const RegisterForm = () => {
         <h2>Create Account On HotelHub</h2>
         <span>Welcome, Sign up to get started!</span>
 
-        <div className="flex gap-3 justify-center items-center pt-10">
-          <Field>
-            <FieldLabel>First Name</FieldLabel>
-            <Input placeholder="First Name" />
-          </Field>
-          <Field>
-            <FieldLabel>Last Name</FieldLabel>
-            <Input placeholder="Last Name" />
-          </Field>
-        </div>
+        <form className="p-1 md:p-2" onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex gap-3 justify-center items-center pt-10">
+            <Field>
+              <FieldLabel>First Name</FieldLabel>
+              <Input
+                placeholder="First Name"
+                type="text"
+                {...register("firstName")}
+              />
+              {errors.lastName && (
+                <p className="text-start text-destructive text-sm">
+                  {errors.firstName?.message}
+                </p>
+              )}
+            </Field>
+            <Field>
+              <FieldLabel>Last Name</FieldLabel>
+              <Input
+                placeholder="Last Name"
+                type="text"
+                {...register("lastName")}
+              />
+              {errors.lastName && (
+                <p className="text-start text-destructive text-sm">
+                  {errors.lastName?.message}
+                </p>
+              )}
+            </Field>
+          </div>
 
-        <div className="flex flex-col py-5 gap-5">
-          <Field>
-            <FieldLabel>Email</FieldLabel>
-            <Input placeholder="Email" />
-          </Field>
-          <Field>
-            <FieldLabel>Password</FieldLabel>
-            <Input placeholder="••••••••" type="password" />
-          </Field>
-        </div>
+          <div className="flex flex-col py-5 gap-5">
+            <Field>
+              <FieldLabel>Email</FieldLabel>
+              <Input placeholder="Email" type="text" {...register("email")} />
+              {errors.lastName && (
+                <p className="text-start text-destructive text-sm">
+                  {errors.email?.message}
+                </p>
+              )}
+            </Field>
+            <Field>
+              <FieldLabel>Password</FieldLabel>
+              <Input
+                placeholder="••••••••"
+                type="password"
+                {...register("password")}
+              />
+              {errors.lastName && (
+                <p className="text-start text-destructive text-sm">
+                  {errors.password?.message}
+                </p>
+              )}
+            </Field>
+          </div>
 
-        <Button className="mt-5">Create Account</Button>
+          <Button className="mt-5 w-full" type="submit" disabled={isSubmitting}>
+            Create Account
+          </Button>
+        </form>
         <div className="flex justify-center items-center gap-2 py-2">
           <span className="font-thin text-muted-foreground">
             Already have account?{" "}
@@ -57,7 +117,7 @@ const RegisterForm = () => {
         <Image
           src="/hotel-3d.png"
           alt=""
-          className="object-cover mx-auto p-3"
+          className="object-cover"
           width={300}
           height={300}
         />
